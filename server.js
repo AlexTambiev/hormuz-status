8bea4c0b091defc27d9aab5af205694d85429287
Alex Tambiev <alexandertambiev@gmail.com>
2026-04-23 11:02:25 +0100

    Added favicon

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const MIME_TYPES = {
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".png": "image/png",
   ".svg": "image/svg+xml",
   ".ico": "image/x-icon",
 };
